2515e565a733da433172444a909e2e8bb99e6322
LOCO185 <jloco151@gmail.com>
2022-04-04 13:16:03 +0300

    posts component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { FavoriteComponent } from '../app/components/favorite/favorite.component';
 import { TitleCasePipe } from './title-case.pipe';
 import { LikeComponent } from './like/like.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FavoriteComponent,
     TitleCasePipe,
-    LikeComponent
+    LikeComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
