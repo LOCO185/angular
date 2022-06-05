119b07ae155c959f43e95778582d481160f88f27
LOCO185 <jloco151@gmail.com>
2022-06-05 23:27:55 +0300

    deletd and fixed some bugs

--- src/app/components/favorite/favorite.component.ts
+++ src/app/components/favorite/favorite.component.ts
@@ -21,10 +21,6 @@ export class FavoriteComponent {
     this.isFavorite = !this.isFavorite;
     this.click.emit({ newValue: this.isFavorite });
   }
-  // onClick() {
-  //   this.isFavorite = !this.isFavorite;
-  //   this.click.emit({ newValue: this.isFavorite });
-  // }
 }
 
 export interface FavoriteChangedEventArgs {
