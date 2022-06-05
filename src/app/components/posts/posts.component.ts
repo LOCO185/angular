a432a018ccbe10f3165a2abbca638cb618335bc8
LOCO185 <jloco151@gmail.com>
2022-06-05 23:24:12 +0300

    deleted comment on posts

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -28,8 +28,6 @@ export class PostsComponent implements OnInit {
       })
   }
   updatePost(post:any) {
-    // this.http.put(this.url, JSON.stringify(post))
-    // the whole object with PUT.
     this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true}))
     .subscribe(response => {
       console.log(response);
